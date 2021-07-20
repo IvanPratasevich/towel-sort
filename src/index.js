3888e13090c84f4a7969d3140b686920cfa31c20
IvanPratasevich <iv.pratasevich@gmail.com>
2021-07-20 21:59:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+if (matrix === undefined || matrix.length === 0){
+    return []
+  }
+let matrixI = [];
+let matrixN = matrix;
+matrixN.unshift("p");
+matrixN.reduce(function(previousValue, item, index, array) {
+if (index % 2 === 0){
+      item = item.reverse().concat(previousValue);
+    } else{
+      item = item;
+    }
+},)
+matrixN.splice(0, 1);
+return matrixN.join().split(",")
 }
